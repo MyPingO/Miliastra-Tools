69321bb582846088a1ba5bac51144c08b37f8919
Ping <81280020+MyPingO@users.noreply.github.com>
2026-09-05 02:15:06 -0400

    Bump Status Display module revision

--- assets/status-display.js
+++ assets/status-display.js
@@ -3,7 +3,7 @@
 const base='https://mypingo.github.io/Miliastra-Tools/assets/status-v2.part';
 (async()=>{
   try{
-    const responses=await Promise.all([1,2,3,4,5,6].map(i=>fetch(`${base}${i}.txt?v=20260904c`,{cache:'no-store'})));
+    const responses=await Promise.all([1,2,3,4,5,6].map(i=>fetch(`${base}${i}.txt?v=20260905a`,{cache:'no-store'})));
     if(responses.some(r=>!r.ok))throw new Error('Failed to load Status Display editor modules');
     const code=(await Promise.all(responses.map(r=>r.text()))).join('');
     new Function(code)();
